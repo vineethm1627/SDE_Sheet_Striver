bb9128e891ed4db8ace64987fbd9507e56ab348d
M Vineeth <44038287+vineethm1627@users.noreply.github.com>
2021-06-02 20:23:44 +0530

    kadanes Algorithm

--- Day-01_Arrays/README.md
+++ Day-01_Arrays/README.md
@@ -4,3 +4,4 @@ SNo | Name | Logic Used | Link |
 ----|------|------------|------|
 1 | DNF Sort | 3 pointer partitioning | [view](DNF_sort.cpp)
 2 | Finding Missing and Repeating | repeating xor {1 to n} | [view](missing_repeating.cpp)
+3 | Kadane's Algorithm | Max Subarray Sum | [view](kadanes_algorithm.cpp)
---
+++ Day-01_Arrays/kadanes_algorithm.cpp
@@ -0,0 +1,18 @@
+/*
+    Problem Link: https://leetcode.com/problems/maximum-subarray/
+*/
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        // kadane's algo
+        int cur_sum = 0, max_sum = INT_MIN;
+        for(const int x : nums) {
+            cur_sum += x;
+            max_sum = max(max_sum, cur_sum);
+            if(cur_sum < 0)
+                cur_sum = 0;
+        }
+        return max_sum;
+    }
+};
