7ed1445ba37ee9f124d42639dbee5a7ca8747e00
M Vineeth <44038287+vineethm1627@users.noreply.github.com>
2021-06-04 16:54:47 +0530

    subarray with 0 sum

--- Day-04_Hashing/README.md
+++ Day-04_Hashing/README.md
@@ -5,3 +5,4 @@ SNo | Name | Logic Used | Link |
 1 | Two Sum | Hashmap of elements | [view](Two_sum.cpp)
 2 | Longest Consecutive Sequence | unordered set | [view](LongestConsecutiveSequence.cpp)
 3 | Longest Substring without Repeating Chars | sliding window + hashmap | [view](repeated_chars.cpp)
+4 | Largest Subarray with 0 sum | hashmap {cum_sum, i} | [view](subarray_zero_sum.cpp)
---
+++ Day-04_Hashing/subarray_zero_sum.cpp
@@ -0,0 +1,23 @@
+/*
+    Problem Link: https://practice.geeksforgeeks.org/problems/largest-subarray-with-0-sum/1
+*/
+
+int maxLen(int arr[], int n) {
+    // hashmap of cumulative sum --> index
+    unordered_map<int, int> m;
+    int max_len = 0, cur_sum = 0;
+
+    for(int i = 0; i < n; i++) {
+        cur_sum += arr[i];
+        if(cur_sum = 0) {
+            max_len = i + 1;
+            continue;
+        }
+        // look for cur_sum in the hashmap
+        if(m.find(cur_sum) != m.end()) 
+            max_len = max(max_len, i - m[cur_sum]);
+        else
+            m[cur_sum] = i;
+    }
+    return max_len;
+}
